435d8e7398f285f5d5f8cbbaee6281c9dabacd4e
Santiago13-glitch <karinalandi@gmail.com>
2024-07-10 18:31:19 -0500

    Update bot_sup_image.py

--- bot_sup.py
+++ bot_sup.py
@@ -1,5 +1,8 @@
 import discord
+import os
 from discord.ext import commands
+import random
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -11,7 +14,7 @@ async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 @bot.command()
-async def hello(ctx):
+async def hola(ctx):
     await ctx.send(f'Hola, soy un bot {bot.user}!')
 
 @bot.command()
@@ -37,5 +40,35 @@ async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
 
+meme = os.listdir("images")
+
+@bot.command()
+async def mem(ctx):
+    img_name = random.choice(meme)
+    with open(f'images/{img_name}', 'rb') as f:
+            picture = discord.File(f)
+    await ctx.send(file = picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+def get_dog_image_url():    
+    url = "https://random.dog/woof.json"
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('dog')
+async def dog(ctx):
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
 
 bot.run("")
